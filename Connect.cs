1ae59cb6d6a0690391de43a60a9a5081d557f503
IZsombor <ivadizs@kkszki.hu>
2023-10-05 08:07:56 +0200

    Everything done exept the controller

--- Connect.cs
+++ Connect.cs
@@ -1,4 +1,22 @@
-﻿namespace JegyekAPI {
+﻿using MySql.Data.MySqlClient;
+
+namespace JegyekAPI {
     public class Connect {
+        public MySqlConnection connection;
+        private string Host;
+        private string DbName;
+        private string UserName;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect() {
+            Host = "localhost";
+            DbName = "db_jegyek";
+            UserName = "root";
+            Password = "";
+
+            ConnectionString = $"Host={Host};Database={DbName};User={UserName};Password={Password};Ssl Mode=None";
+            connection = new MySqlConnection(ConnectionString);
+        }
     }
 }
