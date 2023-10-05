d5c951d94d253310992837030945fe46c00aacd0
IZsombor <ivadizs@kkszki.hu>
2023-10-05 08:35:33 +0200

    HTTP Get(Id) method implemented

--- Controllers/JegyekController.cs
+++ Controllers/JegyekController.cs
@@ -20,7 +20,6 @@ namespace JegyekAPI.Controllers {
                 string sql = "SELECT * FROM grades";
 
                 MySqlCommand cmd = new MySqlCommand(sql, connect.connection);
-
                 MySqlDataReader reader = cmd.ExecuteReader();
 
                 while (reader.Read()) {
@@ -41,6 +40,42 @@ namespace JegyekAPI.Controllers {
             catch (Exception e) {
 
                 return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<GradeDto> Get(Guid id) {
+            try {
+                connect.connection.Open();
+
+                string sql = "SELECT * FROM grades WHERE Id=@Id";
+
+                MySqlCommand cmd = new MySqlCommand(sql, connect.connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                cmd.Parameters.AddWithValue("Id", id);
+
+                if (reader.Read()) {
+                    var result = new GradeDto(
+                        reader.GetGuid("Id"),
+                        reader.GetInt32("Grade"),
+                        reader.GetString("Description"),
+                        reader.GetDateTime("Created")
+                        );
+
+                    connect.connection.Close();
+                    return StatusCode(200, result);
+                } else {
+                    Exception e = new Exception();
+
+                    connect.connection.Close();
+                    return StatusCode(404, e.Message);
+                }
+
+            }
+            catch (Exception) {
+
+                return BadRequest(404);
             }
         }
     }
